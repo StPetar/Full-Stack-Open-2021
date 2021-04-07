7145156476aa7faf50906f02febaee866bc235c1
StPetar <petarst97@gmail.com>
2021-04-07 21:17:22 +0300

    display statistics only after feedback

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -14,10 +14,7 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       
       <h1>Statistics</h1>
-      <Display value={good} text='good' />
-      <Display value={neutral} text='neutral' />
-      <Display value={bad} text='bad'/>
-      
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
@@ -33,4 +30,29 @@ const Display = ({text, value}) => (
     {text} {value}
   </div>
 )
+
+const Statistics = ({good, neutral, bad}) => {
+  const numOfReviews = good + neutral + bad
+  if (numOfReviews === 0){
+    return(
+      <div>
+        <p>No Feedback given</p>
+      </div>
+    )
+  }
+  // Per definition (good: 1, neutral: 0, bad: -1)
+  // neutral can be omitted
+  const avg = (good - bad) / numOfReviews
+  const rating = (good / numOfReviews) * 100
+  return(
+    <div>
+      <Display value={good} text='good' />
+      <Display value={neutral} text='neutral' />
+      <Display value={bad} text='bad'/>
+      Total reviews {numOfReviews}<br/>
+      Average {avg}<br/>
+      Rating {rating} %<br/>
+    </div>
+  )
+}
 export default App
