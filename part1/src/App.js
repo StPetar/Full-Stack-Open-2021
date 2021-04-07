a2768ece2f4ac56fc0af8b16c45190a3927e63a3
StPetar <petarst97@gmail.com>
2021-04-07 20:12:42 +0300

    exercise 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
-
-  const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
-
-const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      
+      <h1>Statistics</h1>
+      <Display value={good} text='good' />
+      <Display value={neutral} text='neutral' />
+      <Display value={bad} text='bad'/>
+      
     </div>
   )
 }
 
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}  
+  </button>
+)
+
+const Display = ({text, value}) => (
+  <div>
+    {text} {value}
+  </div>
+)
 export default App
