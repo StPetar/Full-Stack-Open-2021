b2610e40822eab4c5961e8871bd20d3f18a28a47
Petar Stoyanov <57144484+StPetar@users.noreply.github.com>
2021-04-23 14:28:18 +0300

    Delete part2 directory

--- part2/countries/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({ search, handleSearch }) => (
-  <div>
-    Find Countries: <input value={search} onChange={handleSearch} />
-  </div>
-)
-
-export default Filter
--- part2/courseinfo/src/components/Part.js
+++
@@ -1,9 +0,0 @@
-const Part = ({part, content}) => {
-  return(
-    <div>
-      <p>{part} {content}</p>
-    </div>
-  )
-}
-
-export default Part;
